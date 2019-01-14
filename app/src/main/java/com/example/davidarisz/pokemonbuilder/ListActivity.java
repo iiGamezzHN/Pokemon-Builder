e7bb27799b216a08445666cddb5eb8159e496eff
David Arisz <davidarisz@gmail.com>
2019-01-14 15:35:36 +0100

    Made ListDetailsActivity, it shows the saved pokemon details

--- app/src/main/java/com/example/davidarisz/pokemonbuilder/ListActivity.java
+++ app/src/main/java/com/example/davidarisz/pokemonbuilder/ListActivity.java
@@ -1,10 +1,12 @@
 package com.example.davidarisz.pokemonbuilder;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -15,6 +17,7 @@ public class ListActivity extends AppCompatActivity implements PokemonRequest.Ca
     private ArrayList pokemonNames;
     private PokemonDatabase db;
     private ListAdapter adapter;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +32,49 @@ public class ListActivity extends AppCompatActivity implements PokemonRequest.Ca
 
         ListView listView = findViewById(R.id.lv_saved_pokemon);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new ListViewClickListener());
 
         Button button = findViewById(R.id.btn_list_tab);
         button.setBackgroundColor(getResources().getColor(R.color.selectedTab));
+    }
+
+    private class ListViewClickListener implements AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+            // Do something
+            db = PokemonDatabase.getInstance(getApplicationContext());
+            cursor = (Cursor) adapterView.getItemAtPosition(position);
+
+            String name = cursor.getString(cursor.getColumnIndex("name"));
+            String item = cursor.getString(cursor.getColumnIndex("item"));
+            String ability = cursor.getString(cursor.getColumnIndex("ability"));
+            String move1 = cursor.getString(cursor.getColumnIndex("move1"));
+            String move2 = cursor.getString(cursor.getColumnIndex("move2"));
+            String move3 = cursor.getString(cursor.getColumnIndex("move3"));
+            String move4 = cursor.getString(cursor.getColumnIndex("move4"));
+            String nature = cursor.getString(cursor.getColumnIndex("nature"));
+            int hp_iv = cursor.getInt(cursor.getColumnIndex("hp_iv"));
+            int att_iv = cursor.getInt(cursor.getColumnIndex("att_iv"));
+            int def_iv = cursor.getInt(cursor.getColumnIndex("def_iv"));
+            int spa_iv = cursor.getInt(cursor.getColumnIndex("spa_iv"));
+            int spd_iv = cursor.getInt(cursor.getColumnIndex("spd_iv"));
+            int sp_iv = cursor.getInt(cursor.getColumnIndex("sp_iv"));
+            int hp_ev = cursor.getInt(cursor.getColumnIndex("hp_ev"));
+            int att_ev = cursor.getInt(cursor.getColumnIndex("att_ev"));
+            int def_ev = cursor.getInt(cursor.getColumnIndex("def_ev"));
+            int spa_ev = cursor.getInt(cursor.getColumnIndex("spa_ev"));
+            int spd_ev = cursor.getInt(cursor.getColumnIndex("spd_ev"));
+            int sp_ev = cursor.getInt(cursor.getColumnIndex("sp_ev"));
+            String url = cursor.getString(cursor.getColumnIndex("url"));
+
+            SavedPokemon savedPokemon = new SavedPokemon(0,name,item,ability,move1,move2,move3,move4,
+                    nature,hp_iv,att_iv,def_iv,spa_iv,spd_iv,sp_iv,hp_ev,att_ev,def_ev,spa_ev,spd_ev,sp_ev,url);
+
+            Intent intent = new Intent(ListActivity.this, ListDetailActivity.class);
+            intent.putExtra("savedTag", savedPokemon);
+            startActivity(intent);
+            overridePendingTransition(0, 0);
+        }
     }
 
     public void gotPokemon(final ArrayList<String> pokemon) {
---
+++ app/src/main/java/com/example/davidarisz/pokemonbuilder/ListDetailActivity.java
@@ -0,0 +1,97 @@
+package com.example.davidarisz.pokemonbuilder;
+
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class ListDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_detail);
+
+        SavedPokemon savedPokemon = (SavedPokemon) getIntent().getSerializableExtra("savedTag");
+
+        String name = savedPokemon.getName();
+        String url = savedPokemon.getUrl();
+        String item = savedPokemon.getItem();
+        String ability = savedPokemon.getAbility();
+        String move1 = savedPokemon.getMove1();
+        String move2 = savedPokemon.getMove2();
+        String move3 = savedPokemon.getMove3();
+        String move4 = savedPokemon.getMove4();
+        String nature = savedPokemon.getNature();
+        int hp_iv = savedPokemon.getHp_iv();
+        int att_iv = savedPokemon.getAtt_iv();
+        int def_iv = savedPokemon.getDef_iv();
+        int spa_iv = savedPokemon.getSpa_iv();
+        int spd_iv = savedPokemon.getSpd_iv();
+        int sp_iv = savedPokemon.getSp_iv();
+        int hp_ev = savedPokemon.getHp_ev();
+        int att_ev = savedPokemon.getAtt_ev();
+        int def_ev = savedPokemon.getDef_ev();
+        int spa_ev = savedPokemon.getSpa_ev();
+        int spd_ev = savedPokemon.getSpd_ev();
+        int sp_ev = savedPokemon.getSp_ev();
+
+        TextView tv_name = findViewById(R.id.tv_name);
+        ImageView img_picture = findViewById(R.id.img_picture);
+        TextView tv_item = findViewById(R.id.tv_item);
+        TextView tv_ability = findViewById(R.id.tv_ability);
+        TextView tv_move1 = findViewById(R.id.tv_move1);
+        TextView tv_move2 = findViewById(R.id.tv_move2);
+        TextView tv_move3 = findViewById(R.id.tv_move3);
+        TextView tv_move4 = findViewById(R.id.tv_move4);
+        TextView tv_nature = findViewById(R.id.tv_nature);
+        TextView tv_hp_iv = findViewById(R.id.tv_hp_iv);
+        TextView tv_att_iv = findViewById(R.id.tv_att_iv);
+        TextView tv_def_iv = findViewById(R.id.tv_def_iv);
+        TextView tv_spa_iv = findViewById(R.id.tv_spa_iv);
+        TextView tv_spd_iv = findViewById(R.id.tv_spd_iv);
+        TextView tv_sp_iv = findViewById(R.id.tv_sp_iv);
+        TextView tv_hp_ev = findViewById(R.id.tv_hp_ev);
+        TextView tv_att_ev = findViewById(R.id.tv_att_ev);
+        TextView tv_def_ev = findViewById(R.id.tv_def_ev);
+        TextView tv_spa_ev = findViewById(R.id.tv_spa_ev);
+        TextView tv_spd_ev = findViewById(R.id.tv_spd_ev);
+        TextView tv_sp_ev = findViewById(R.id.tv_sp_ev);
+
+        tv_name.setText(name);
+        Picasso.get().load(url).into(img_picture);
+        tv_item.setText(item);
+        tv_ability.setText(ability);
+        tv_move1.setText(move1);
+        tv_move2.setText(move2);
+        tv_move3.setText(move3);
+        tv_move4.setText(move4);
+        tv_nature.setText(nature);
+        tv_hp_ev.setText(String.valueOf(hp_ev));
+        tv_att_ev.setText(String.valueOf(att_ev));
+        tv_def_ev.setText(String.valueOf(def_ev));
+        tv_spa_ev.setText(String.valueOf(spa_ev));
+        tv_spd_ev.setText(String.valueOf(spd_ev));
+        tv_sp_ev.setText(String.valueOf(sp_ev));
+        tv_hp_iv.setText(String.valueOf(hp_iv));
+        tv_att_iv.setText(String.valueOf(att_iv));
+        tv_def_iv.setText(String.valueOf(def_iv));
+        tv_spa_iv.setText(String.valueOf(spa_iv));
+        tv_spd_iv.setText(String.valueOf(spd_iv));
+        tv_sp_iv.setText(String.valueOf(sp_iv));
+
+    }
+
+    public void toList(View view) {
+    }
+
+    public void toSearch(View view) {
+    }
+
+    public void toPokedex(View view) {
+    }
+}
