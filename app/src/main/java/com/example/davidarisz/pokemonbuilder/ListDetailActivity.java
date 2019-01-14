2334fb4e5a6b910b34060c383c23b7a02f63f41f
David Arisz <davidarisz@gmail.com>
2019-01-14 15:55:34 +0100

    Small changes to layout of ListDetailActivity

--- app/src/main/java/com/example/davidarisz/pokemonbuilder/ListDetailActivity.java
+++ app/src/main/java/com/example/davidarisz/pokemonbuilder/ListDetailActivity.java
@@ -1,20 +1,27 @@
 package com.example.davidarisz.pokemonbuilder;
 
+import android.content.Intent;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
+
 public class ListDetailActivity extends AppCompatActivity {
+    ArrayList pokemonNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_detail);
+        Intent intent = getIntent();
+        pokemonNames = intent.getStringArrayListExtra("namesTag");
 
         SavedPokemon savedPokemon = (SavedPokemon) getIntent().getSerializableExtra("savedTag");
 
@@ -84,14 +91,28 @@ public class ListDetailActivity extends AppCompatActivity {
         tv_spd_iv.setText(String.valueOf(spd_iv));
         tv_sp_iv.setText(String.valueOf(sp_iv));
 
+        Button button = findViewById(R.id.btn_list_tab);
+        button.setBackgroundColor(getResources().getColor(R.color.selectedTab));
     }
 
     public void toList(View view) {
+        Intent intent = new Intent(this, ListActivity.class);
+        intent.putStringArrayListExtra("namesTag", pokemonNames);
+        startActivity(intent);
+        overridePendingTransition(0, 0);
     }
 
     public void toSearch(View view) {
+        Intent intent = new Intent(this, AddActivity.class);
+        intent.putStringArrayListExtra("namesTag", pokemonNames);
+        startActivity(intent);
+        overridePendingTransition(0, 0);
     }
 
     public void toPokedex(View view) {
+        Intent intent = new Intent(this, PokedexActivity.class);
+        intent.putStringArrayListExtra("namesTag", pokemonNames);
+        startActivity(intent);
+        overridePendingTransition(0, 0);
     }
 }
